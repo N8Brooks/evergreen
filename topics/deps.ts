13fe13acef0b4a8d0905f59802048e0dd96d17bd
Nathan Brooks <natebrooks004@gmail.com>
2021-11-20 16:38:01 -0800

    fix: external reference to common

--- topics/deps.ts
+++ topics/deps.ts
@@ -1,3 +1,3 @@
-export * from "../common/mod.ts";
+export * from "https://raw.githubusercontent.com/N8Brooks/evergreen/main/common/mod.ts";
 export * from "https://deno.land/x/oak@v10.0.0/mod.ts";
 export * from "https://deno.land/x/mongo@v0.28.0/mod.ts";
