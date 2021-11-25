6a33c1db9e5978ec9b6a54c84380a15072831426
Nathan Brooks <natebrooks004@gmail.com>
2021-11-24 22:31:28 -0800

    fix: remove no vote from vote directions

--- common/util/vote_sort_keys_builder.ts
+++ common/util/vote_sort_keys_builder.ts
@@ -11,7 +11,7 @@ const Z_SCORE_2 = Z_SCORE * Z_SCORE;
 export interface VoteSortKeysBuilderOptions {
   oldDownVotes: number;
   oldUpVotes: number;
-  oldVoteDirection: VoteDirections;
+  oldVoteDirection: Omit<VoteDirections, VoteDirections.NoVote>;
   newVoteDirection: VoteDirections;
 }
 
