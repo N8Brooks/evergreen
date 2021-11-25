065e4e4598172ace730a7be45753871ceb980ea0
Nathan Brooks <natebrooks004@gmail.com>
2021-11-25 14:17:09 -0800

    feat: unique user index

--- users/models/users.ts
+++ users/models/users.ts
@@ -1,3 +1,4 @@
+import { IndexOptions } from "../deps.ts";
 import { mongoClient } from "./mongo_client.ts";
 
 const db = mongoClient.database("users");
@@ -12,4 +13,17 @@ export interface UserSchema {
   name: string;
 }
 
-export const users = db.collection<UserSchema>("users");
+const users = db.collection<UserSchema>("users");
+
+/** User names must be unique */
+const nameIndex: IndexOptions = {
+  key: { name: 1 },
+  name: "_name",
+  unique: true,
+};
+
+users.createIndexes({
+  indexes: [nameIndex],
+});
+
+export { users };
