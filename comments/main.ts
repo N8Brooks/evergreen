92a85eb8ea94123d9ff22c4dcc1b48fc9401d4ce
Nathan Brooks <natebrooks004@gmail.com>
2021-11-24 22:42:47 -0800

    fix: remove test subscriber

--- comments/main.ts
+++ comments/main.ts
@@ -1,13 +1,9 @@
 import { Application } from "./deps.ts";
-import { natsConnection } from "./events/nats_connection.ts";
-import { SubmissionCreatedSubscriber } from "./events/submission_created_subscriber.ts";
 import { createCommentCommentRouter } from "./routes/comments/create.ts";
 import { listCommentCommentsRouter } from "./routes/comments/list.ts";
 import { voteCommentRouter } from "./routes/comments/vote.ts";
 import { createSubmissionCommentRouter } from "./routes/submissions/create.ts";
 import { listSubmissionCommentsRouter } from "./routes/submissions/list.ts";
-
-new SubmissionCreatedSubscriber(natsConnection).listen();
 
 const app = new Application();
 
