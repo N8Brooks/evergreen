1ac50a7078aca8a46318ef660ac72d1849f97df2
Nathan Brooks <natebrooks004@gmail.com>
2021-11-25 19:54:39 -0800

    update common

--- common/events/comment_voted_event.ts
+++ common/events/comment_voted_event.ts
@@ -1,5 +1,4 @@
 import { Subjects } from "./subjects.ts";
-import { VoteDirections } from "../types/vote_directions.ts";
 import { Event } from "./event.ts";
 
 export interface CommentVotedEvent extends Event {
