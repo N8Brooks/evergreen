335490fe1d4d6409700f3af3c15062bb4599d8c4
Nathan Brooks <natebrooks004@gmail.com>
2021-11-25 18:32:10 -0800

    fix: export comment events

--- common/mod.ts
+++ common/mod.ts
@@ -1,13 +1,19 @@
-export * from "./events/subscriber.ts";
+// events
+export * from "./events/comment_created_event.ts";
+export * from "./events/comment_voted_event.ts";
 export * from "./events/event.ts";
 export * from "./events/publisher.ts";
 export * from "./events/subjects.ts";
 export * from "./events/submission_created_event.ts";
 export * from "./events/submission_voted_event.ts";
+export * from "./events/subscriber.ts";
 export * from "./events/topic_created_event.ts";
 export * from "./events/user_created_event.ts";
+// models
+export * from "./models/connect_mongo_client_with_retries.ts";
+// types
 export * from "./types/languages.ts";
 export * from "./types/vote_directions.ts";
 export * from "./types/vote_sort_keys.ts";
-export * from "./models/connect_mongo_client_with_retries.ts";
+// util
 export * from "./util/vote_sort_keys_builder.ts";
