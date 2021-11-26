eee64bb24492dcbf8f008bc9e87ce073f55238da
Nathan Brooks <natebrooks004@gmail.com>
2021-11-25 17:25:35 -0800

    fix: add created to user created event

--- common/events/user_created_event.ts
+++ common/events/user_created_event.ts
@@ -4,7 +4,11 @@ import { Subjects } from "./subjects.ts";
 export interface UserCreatedEvent extends Event {
   subject: Subjects.UserCreated;
   message: {
+    /** User id */
     id: string;
+
+    /** Creation date */
+    createdAt: Date;
 
     /** User name */
     name: string;
