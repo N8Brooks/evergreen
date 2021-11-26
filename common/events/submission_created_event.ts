c7860c5bfb24743fa0b2e9e772ef07c83bf9c92b
Nathan Brooks <natebrooks004@gmail.com>
2021-11-25 17:11:24 -0800

    ref: refactor events

--- common/events/submission_created_event.ts
+++ common/events/submission_created_event.ts
@@ -1,13 +1,29 @@
 import { Event } from "./event.ts";
 import { Subjects } from "./subjects.ts";
+import { Languages } from "../types/languages.ts";
 
 export interface SubmissionCreatedEvent extends Event {
   subject: Subjects.SubmissionCreated;
   message: {
+    /** Submission id */
     id: string;
+
+    /** Creation date */
+    createdAt: Date;
+
+    /** Iso 639-1 code */
+    language: Languages;
+
+    /** Reference to topic */
     topicId: string;
+
+    /** Author */
     userId: string;
-    title: string;
+
+    /** Given title */
+    name: string;
+
+    /** Associated url */
     url?: string;
   };
 }
--- common/events/topic_created_event.ts
+++ common/events/topic_created_event.ts
@@ -4,7 +4,11 @@ import { Subjects } from "./subjects.ts";
 export interface TopicCreatedEvent extends Event {
   subject: Subjects.TopicCreated;
   message: {
+    /** Topic id */
     id: string;
+
+    /** Creation date */
+    createdAt: Date;
 
     /** Topic name */
     name: string;
