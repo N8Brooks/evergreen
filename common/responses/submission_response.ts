1248883ed9ade7bad6b22d14eb1ce4d462b0883f
Nathan Brooks <natebrooks004@gmail.com>
2021-11-28 17:19:49 -0800

    ref(common): move submission response and update

--- common/responses/submission_response.ts
+++ common/responses/submission_response.ts
@@ -1,8 +1,10 @@
+/** Standard response from submissions service */
 export interface SubmissionResponse {
   title: string;
   url: string;
   score: number;
   userName: string;
   topicName: string;
-  createdAt: string;
+  createdAt: number;
+  commentCount: number;
 }
