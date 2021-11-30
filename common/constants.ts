a266f6bc37d7f561d8ead2e9ecd2fc5b64c41244
Nathan Brooks <natebrooks004@gmail.com>
2021-11-29 20:54:41 -0800

    ref(common): switch back to cookie user name

--- common/constants.ts
+++ common/constants.ts
@@ -1,5 +1,5 @@
 /** The key used when setting or getting the cookie */
-export const COOKIE_USER_ID = "user-id";
+export const COOKIE_USER_NAME = "user-name";
 
 /** Name pattern for users and topics */
 export const NAME_PATTERN = /^\w{1,32}$/;
