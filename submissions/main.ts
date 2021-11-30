165de7a90b337a05330e40ef95e2207c4795209e
Nathan Brooks <natebrooks004@gmail.com>
2021-11-29 22:53:40 -0800

    ref(submissions): vote on submission in route

--- submissions/main.ts
+++ submissions/main.ts
@@ -1,4 +1,3 @@
-import { voteSubmissionRouter } from "./routes/topics/vote.ts";
 import { Application, errorHandler, log, Router } from "./deps.ts";
 import { topicCreatedSubscriber } from "./events/topic_created_subscriber.ts";
 import { listSubmissionsRouter } from "./routes/list.ts";
@@ -8,19 +7,20 @@ import { listSubmissionsByUpVotedRouter } from "./routes/users/up_voted.ts";
 import { commentCreatedSubscriber } from "./events/comment_created_subscriber.ts";
 import { listSubmissionInTopicRoute } from "./routes/topics/list_submissions_in_topic_route.ts";
 import { createSubmissionInTopicRoute } from "./routes/topics/create_submission_in_topic_route.ts";
+import { voteOnSubmissionRoute } from "./routes/vote_on_submission_route.ts";
 
 commentCreatedSubscriber.listen();
 topicCreatedSubscriber.listen();
 
 const submissionsRouter = new Router()
   .post("/api/topics/:topicName/submissions", createSubmissionInTopicRoute)
-  .get("/api/topics/:topicName/submissions", listSubmissionInTopicRoute);
+  .get("/api/topics/:topicName/submissions", listSubmissionInTopicRoute)
+  .patch("/api/submissions/:submissionId", voteOnSubmissionRoute);
 
 const app = new Application()
   .use(errorHandler)
   .use(submissionsRouter.routes());
 
-app.use(voteSubmissionRouter.routes());
 app.use(listSubmissionsRouter.routes());
 app.use(listSubmissionsByUpVotedRouter.routes());
 app.use(listSubmissionsByDownVotedRouter.routes());
