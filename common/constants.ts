61ca8551fc2f0f213e96ad5b4addbad18a1c99d0
Nathan Brooks <natebrooks004@gmail.com>
2021-11-29 22:10:45 -0800

    ref(common): unicode regexp

--- common/constants.ts
+++ common/constants.ts
@@ -2,7 +2,7 @@
 export const COOKIE_USER_NAME = "user-name";
 
 /** Name pattern for users and topics */
-export const NAME_PATTERN = /^\w{1,32}$/;
+export const NAME_PATTERN = /^\w{1,32}$/u;
 
 /** Matches URLs between 4 and 2048 character inclusive with no non-escaped characters */
-export const URI_PATTERN = /^[\w;,/\?:@\&=\+\$\-\.!~\*'\(\)#]{4,2048}$/;
+export const URI_PATTERN = /^[\w;,/\?:@&=\+\$\-\.!~\*'\(\)#]{4,2048}$/u;
