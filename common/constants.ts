1471d10441e2b52babbf2f565ee4e3af3ef8f3c6
Nathan Brooks <natebrooks004@gmail.com>
2021-11-29 21:36:24 -0800

    ref(common): add uri pattern to constants

--- common/constants.ts
+++ common/constants.ts
@@ -3,3 +3,6 @@ export const COOKIE_USER_NAME = "user-name";
 
 /** Name pattern for users and topics */
 export const NAME_PATTERN = /^\w{1,32}$/;
+
+/** Matches URLs between 4 and 2048 character inclusive with no non-escaped characters */
+export const URI_PATTERN = /^[\w;,/\?:@\&=\+\$\-\.!~\*'\(\)#]{4,2048}$/;
