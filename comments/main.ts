e536d5f4d22152119e3e141890f1f5cf0ded4b57
Nathan Brooks <natebrooks004@gmail.com>
2021-11-30 23:45:31 -0800

    ref(comments): list comments for comment

--- comments/main.ts
+++ comments/main.ts
@@ -1,7 +1,7 @@
 import { Application, errorHandler, log, Router } from "./deps.ts";
 import { submissionCreatedSubscriber } from "./events/submission_created_subscriber.ts";
 import { createCommentForComment } from "./routes/comments/create_comment_for_comment.ts";
-import { listCommentCommentsRouter } from "./routes/comments/list.ts";
+import { listCommentsForComment } from "./routes/comments/list_comments_for_comment.ts";
 import { voteCommentRouter } from "./routes/comments/vote.ts";
 import { createSubmissionCommentRouter } from "./routes/submissions/create.ts";
 import { listSubmissionCommentsRouter } from "./routes/submissions/list.ts";
@@ -9,13 +9,13 @@ import { listSubmissionCommentsRouter } from "./routes/submissions/list.ts";
 submissionCreatedSubscriber.listen();
 
 const commentRouter = new Router()
-  .post("/api/comments/:commentId/comments", createCommentForComment);
+  .post("/api/comments/:commentId/comments", createCommentForComment)
+  .get("/api/comments/:commentId/comments", listCommentsForComment);
 
 const app = new Application()
   .use(errorHandler)
   .use(commentRouter.routes());
 
-app.use(listCommentCommentsRouter.routes());
 app.use(voteCommentRouter.routes());
 app.use(createSubmissionCommentRouter.routes());
 app.use(listSubmissionCommentsRouter.routes());
---
+++ comments/routes/comments/list_comments_for_comment.ts
@@ -0,0 +1,22 @@
+import { Bson, httpErrors, RouterContext } from "../../deps.ts";
+import { comments } from "../../models/comments.ts";
+
+const listCommentsForComment = async (
+  context: RouterContext<"/api/comments/:commentId/comments">,
+) => {
+  // Comment exists or throw 404
+  const commentId = context.params.commentId;
+  const comment = await comments.findOne({ _id: new Bson.ObjectId(commentId) });
+  if (!comment) {
+    throw new httpErrors.NotFound("Comment does not exist");
+  }
+
+  // TODO: truncate results
+
+  // Return 200 with comments
+  context.response.body = await comments
+    .find({ parentId: commentId })
+    .toArray();
+};
+
+export { listCommentsForComment };
